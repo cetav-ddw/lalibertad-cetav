84bd9d3b1898ac542086df5788f8b3eb1c5f1e6e
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-10-26 19:13:30 -0600

    navigation-2

--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -1,36 +1,83 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+import { Box, Link, Image } from '@chakra-ui/react';
+import { Link as NextLink } from 'next/link';
 
 const Navigation = () => {
   return (
-    <Breadcrumb separator="-">
-      <BreadcrumbItem>
-        <BreadcrumbLink href="/inicio">Inicio</BreadcrumbLink>
-      </BreadcrumbItem>
+    <Box
+      as="nav"
+      display="flex"
+      alignItems="center"
+      p={4}
+      bg="blue.500"
+      justifyContent="space-between"
+    >
+      <Box display="flex" flex="1" justifyContent="center">
+        <Image src="/logo.png" alt="" h={8} w={8} mr={4} />
 
-      <BreadcrumbItem>
-        <BreadcrumbLink href="/cursos-libres">Cursos libres</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem>
-        <BreadcrumbLink href="/tecnicos">Cursos Tecnicos</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink href="/about-cetav">Sobre nosotros</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink href="/egresados">Egresados</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink href="/preguntas-frecuentes">FAQ</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink href="/contacto">Contacto</BreadcrumbLink>
-      </BreadcrumbItem>
-    </Breadcrumb>
+        <Link
+          as={NextLink}
+          href="/"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          Inicio
+        </Link>
+        <Link
+          as={NextLink}
+          href="/cursos-libres"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          Cursos Libres
+        </Link>
+        <Link
+          as={NextLink}
+          href="/tecnicos"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          Cursos Técnicos
+        </Link>
+        <Link
+          as={NextLink}
+          href="/about-cetav"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          Sobre Nosotros
+        </Link>
+        <Link
+          as={NextLink}
+          href="/egresados"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          Egresados
+        </Link>
+        <Link
+          as={NextLink}
+          href="/preguntas-frecuentes"
+          color="white"
+          _hover={{ color: 'white' }}
+          mr={4}
+        >
+          FAQ
+        </Link>
+        <Link
+          as={NextLink}
+          href="/contacto"
+          color="white"
+          _hover={{ color: 'white' }}
+        >
+          Contacto
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <main>
       <h1>HOME page</h1>
-      <Link href="/">HOME</Link>
     </main>
   );
 }
