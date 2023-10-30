15a87ba3ef0f6473f6fd3d5319e0bcf624e1a34d
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-10-30 14:10:55 -0600

    Filter-term-list-Done

---
+++ src/app/egresados/components/FormHandler.jsx
@@ -0,0 +1,28 @@
+let carreerList = [];
+let yearList = [];
+
+export const handleOptionClick = (value, formId) => {
+  if (formId === 'form1') {
+    if (carreerList.includes(value)) {
+      // Si está seleccionado, lo eliminamos de la lista
+      carreerList = carreerList.filter((v) => v !== value);
+    } else {
+      // Si no está seleccionado, lo agregamos a la lista
+      carreerList.push(value);
+    }
+  }else{
+    const intValue = parseInt(value);
+    if (yearList.includes(intValue)) {
+      // Si está seleccionado, lo eliminamos de la lista
+      yearList = yearList.filter((v) => v !== intValue);
+    } else {
+      // Si no está seleccionado, lo agregamos a la lista
+      yearList.push(intValue);
+    }
+  }
+
+
+
+console.log(`Técnicos seleccionados: ${carreerList}`);
+console.log(`Generaciones seleccionadas: ${yearList}`);
+};
--- src/app/egresados/page.js
+++ src/app/egresados/page.js
@@ -1,5 +1,41 @@
-import { Heading } from '@chakra-ui/react';
-
+'use client';
+import {
+  Heading,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuItemOption,
+} from '@chakra-ui/react';
+import { handleOptionClick } from "./components/FormHandler.jsx";
 export default function Egresados() {
-  return <Heading as="h1">EGRESADOS PAGE</Heading>;
+  return <>
+    <Heading as="h1">EGRESADOS PAGE</Heading>
+    <Menu closeOnSelect={false}>
+        <MenuButton as={Button} colorScheme='blue'>Carreras</MenuButton>
+        <MenuList minWidth='240px'>
+          <MenuOptionGroup title='Técnicos' type='checkbox'>
+            <MenuItemOption value='Post-Producción' onClick={() => handleOptionClick('Post-Producción', 'form1')}>Post-Producción</MenuItemOption>
+            <MenuItemOption value='Web' onClick={() => handleOptionClick('Desarrollo Web', 'form1')}>Desarrollo Web</MenuItemOption>
+            <MenuItemOption value='Imagen Comercial' onClick={() => handleOptionClick('Imagen Comercial', 'form1')}>Imagen Comercial</MenuItemOption>
+            <MenuItemOption value='Animación 3D' onClick={() => handleOptionClick('Animación 3D', 'form1')}>Animación 3D</MenuItemOption>
+          </MenuOptionGroup>
+        </MenuList>
+      </Menu>
+      <Menu closeOnSelect={false}>
+        <MenuButton as={Button} colorScheme='blue'>Año</MenuButton>
+        <MenuList minWidth='240px'>
+          <MenuOptionGroup title='Generación' type='checkbox'>
+            <MenuItemOption value='2013' onClick={() => handleOptionClick('2013', 'form2')}>2013</MenuItemOption>
+            <MenuItemOption value='2014' onClick={() => handleOptionClick('2014', 'form2')}>2014</MenuItemOption>
+            <MenuItemOption value='2015' onClick={() => handleOptionClick('2015', 'form2')}>2015</MenuItemOption>
+            <MenuItemOption value='2016' onClick={() => handleOptionClick('2016', 'form2')}>2016</MenuItemOption>
+            <MenuItemOption value='2017' onClick={() => handleOptionClick('2017', 'form2')}>2017</MenuItemOption>
+            <MenuItemOption value='2018' onClick={() => handleOptionClick('2018', 'form2')}>2018</MenuItemOption>
+            <MenuItemOption value='2019' onClick={() => handleOptionClick('2019', 'form2')}>2019</MenuItemOption>
+          </MenuOptionGroup>
+        </MenuList>
+      </Menu>
+  </>
 }
