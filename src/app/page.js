3e62364cbe7b44f3e2ff7f3bbf0ab3d5ee84fd1a
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-10-31 16:55:05 -0600

    feat(formacion): added formacion laboral section (#68)
    Co-authored-by: Jose <leivajd@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Heading } from '@chakra-ui/react';
 import InscriptionBanner from '@/app/components/InscriptionBanner';
+import LaboralFormation from '@/app/components/LaboralFormation';
 
 export default function Home() {
   return <>
   <Heading as="h1">HOME page</Heading>
   <InscriptionBanner/>
+  <LaboralFormation/>
   </>
 }
 
