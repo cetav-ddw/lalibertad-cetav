614f5228ed1c6607d90acc0d6f5742da90e66a38
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-10-31 16:06:37 -0600

    feat(laboral): added corrections

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import { Heading } from '@chakra-ui/react';
 import InscriptionBanner from '@/app/components/InscriptionBanner';
-import FormacionLaboral from '@/app/components/FormacionLaboral';
+import LaboralFormation from '@/app/components/LaboralFormation';
 
 export default function Home() {
   return <>
   <Heading as="h1">HOME page</Heading>
   <InscriptionBanner/>
-  <FormacionLaboral/>
+  <LaboralFormation/>
   </>
 }
 
