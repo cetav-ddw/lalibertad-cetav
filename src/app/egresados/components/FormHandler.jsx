c3c5952ed3566dc467db223a2758b5b056653cbd
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-10-31 16:47:55 -0600

    Move_a_metod_and_the_use_client_to_the_component

--- src/app/egresados/components/FormHandler.jsx
+++ src/app/egresados/components/FormHandler.jsx
@@ -1,7 +1,21 @@
+'use client';
+
+import React from 'react';
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuItemOption,
+  Button,
+} from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+
 let carreerList = [];
 let yearList = [];
 
-export const handleOptionClick = (value, formId) => {
+const handleOptionClick = (value, formId) => {
   if (formId === 'Career Menu') {
     if (carreerList.includes(value)) {
       // Si está seleccionado, lo eliminamos de la lista
@@ -21,3 +35,115 @@ export const handleOptionClick = (value, formId) => {
     }
   }
 };
+
+const FilterMenu = () => {
+
+  return (
+    <Flex justifyContent="center" gap={4} mb={5} mt={5}>
+      <Menu closeOnSelect={false}>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          colorScheme="blue"
+          bg="white"
+          color="black"
+          minWidth="150px"
+        >
+          Técnico
+        </MenuButton>
+        <MenuList minWidth="240px" bg="white" color="black">
+          <MenuOptionGroup title="Técnicos" type="checkbox">
+            <MenuItemOption
+              value="Post-Producción"
+              onClick={() =>
+                handleOptionClick('Post-Producción', 'Career Menu')
+              }
+            >
+              Post-Producción
+            </MenuItemOption>
+            <MenuItemOption
+              value="Web"
+              onClick={() => handleOptionClick('Web', 'Career Menu')}
+            >
+              Desarrollo Web
+            </MenuItemOption>
+            <MenuItemOption
+              value="Imagen Comercial"
+              onClick={() =>
+                handleOptionClick('Imagen Comercial', 'Career Menu')
+              }
+            >
+              Imagen Comercial
+            </MenuItemOption>
+            <MenuItemOption
+              value="Animación 3D"
+              onClick={() => handleOptionClick('Animación', 'Career Menu')}
+            >
+              Animación 3D
+            </MenuItemOption>
+          </MenuOptionGroup>
+        </MenuList>
+      </Menu>
+      <Menu closeOnSelect={false}>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          colorScheme="blue"
+          bg="white"
+          color="black"
+          minWidth="150px"
+        >
+          Generación
+        </MenuButton>
+        <MenuList minWidth="240px" bg="white" color="black">
+          <MenuOptionGroup title="Generación" type="checkbox">
+            <MenuItemOption
+              value="2013"
+              onClick={() => handleOptionClick('2013', 'Year Menu')}
+            >
+              2013
+            </MenuItemOption>
+            <MenuItemOption
+              value="2014"
+              onClick={() => handleOptionClick('2014', 'Year Menu')}
+            >
+              2014
+            </MenuItemOption>
+            <MenuItemOption
+              value="2015"
+              onClick={() => handleOptionClick('2015', 'Year Menu')}
+            >
+              2015
+            </MenuItemOption>
+            <MenuItemOption
+              value="2016"
+              onClick={() => handleOptionClick('2016', 'Year Menu')}
+            >
+              2016
+            </MenuItemOption>
+            <MenuItemOption
+              value="2017"
+              onClick={() => handleOptionClick('2017', 'Year Menu')}
+            >
+              2017
+            </MenuItemOption>
+            <MenuItemOption
+              value="2018"
+              onClick={() => handleOptionClick('2018', 'Year Menu')}
+            >
+              2018
+            </MenuItemOption>
+            <MenuItemOption
+              value="2019"
+              onClick={() => handleOptionClick('2019', 'Year Menu')}
+            >
+              2019
+            </MenuItemOption>
+          </MenuOptionGroup>
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
+
+export default FilterMenu;
