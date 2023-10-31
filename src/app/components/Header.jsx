0f4617f60c7c8da24080531d36d278fe5114cde4
Jason Umaña <jason1202u@gmail.com>
2023-10-31 16:59:16 -0600

    save to update branch

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from './navigation';
 const Header = () => {
   return (
     <Box as="header">
-      <Box maxW="1180px" ml="auto" mr="auto">
+      <Box maxW="100wh" ml="auto" mr="auto">
         <Navigation />
       </Box>
     </Box>
--- src/app/components/listacursos/CursosList.jsx
+++ src/app/components/listacursos/CursosList.jsx
@@ -1,6 +1,5 @@
 import {
   Heading,
-  CardBody,
   Image,
   Wrap,
   WrapItem,
@@ -8,47 +7,39 @@ import {
   Box,
   LinkBox,
   LinkOverlay,
-  Card,
 } from '@chakra-ui/react';
 import SectionHeading from '../SectionHeading';
 import NextLink from 'next/link';
 
-export const ListaCursos = ({ cursos }) => {
+export const ListaCursos = ({ cursos, variante }) => {
   return (
     <>
       {cursos?.length > 0 ? (
         <Box>
           <SectionHeading title="Nuetras Carreras Tecnicas" />
           <Wrap>
-            {cursos.map(({ id, imgSrc, title, descripcion, url, details }) => {
+            {cursos.map(({ id, imgSrc, title, descripcion, url }) => {
               return (
                 <WrapItem key={id}>
-                  <LinkBox>
-                    <Card maxW="sm">
-                      <CardBody p maxW="sm">
-                        <Image src={imgSrc} alt="" borderRadius="lg" w="200" />
-                        <Heading
-                          position="absolute"
-                          left="3"
-                          bottom="150"
-                          size="md"
-                        >
-                          <LinkOverlay as={NextLink} href={url}>
-                            {title}
-                          </LinkOverlay>
-                        </Heading>
-                        <Text
-                          mt="5"
-                          mb="5"
-                          ml="auto"
-                          mr="auto"
-                          textAlign="justify"
-                          width="90%"
-                        >
-                          {descripcion}
-                        </Text>
-                      </CardBody>
-                    </Card>
+                  <LinkBox as="article" maxW="380" borderWidth="1px">
+                    <Box>
+                      <LinkOverlay as={NextLink} href={url}>
+                        <Image src={imgSrc} alt="" borderRadius="lg" />
+                      </LinkOverlay>
+                      <Heading position="absolute" size="md" my="2">
+                        {title}
+                      </Heading>
+                      <Text
+                        mt="5"
+                        mb="5"
+                        ml="auto"
+                        mr="auto"
+                        textAlign="justify"
+                        width="90%"
+                      >
+                        {descripcion}
+                      </Text>
+                    </Box>
                   </LinkBox>
                 </WrapItem>
               );
