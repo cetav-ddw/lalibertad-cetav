9b342e9710162a94322a0985628885457aa58038
Jose <leivajd@gmail.com>
2023-10-30 19:22:24 -0600

    Se incluye wrapper para cada seccion del layout (#42)

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,21 +1,27 @@
-import { Box, Text, Link, Heading, ListItem, UnorderedList } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Link,
+  Heading,
+  ListItem,
+  UnorderedList,
+} from '@chakra-ui/react';
 import { Link as NextLink } from 'next/link';
 
 const Footer = () => {
   return (
-    <Box 
-      as="footer" 
-      p={4} 
-      bg="gray.100" 
-      mx="auto" 
-      maxW="1180px"
-      display="grid"
-      gridTemplateColumns="repeat(4, 1fr)"
-      gap={4}
-    >
-      
-      <Box>
-        <Heading as="h3" size="lg">Conozca más</Heading>
+    <Box as="footer">
+      <Box
+        mx="auto"
+        maxW="1180px"
+        display="grid"
+        gridTemplateColumns="repeat(4, 1fr)"
+        gap={4}
+      >
+        <Box>
+          <Heading as="h3" size="lg">
+            Conozca más
+          </Heading>
           <UnorderedList styleType="none">
             <ListItem>
               <Link as={NextLink} fontSize="sm" href="/about-cetav">
@@ -33,10 +39,12 @@ const Footer = () => {
               </Link>
             </ListItem>
           </UnorderedList>
-      </Box>
+        </Box>
 
-      <Box>
-        <Heading as="h3" size="lg">Carreras</Heading>
+        <Box>
+          <Heading as="h3" size="lg">
+            Carreras
+          </Heading>
           <UnorderedList styleType="none">
             <ListItem>
               <Link as={NextLink} fontSize="sm" href="/tecnicos/dis-y-des-web">
@@ -49,17 +57,29 @@ const Footer = () => {
               </Link>
             </ListItem>
             <ListItem>
-              <Link as={NextLink} fontSize="sm" href="/tecnicos/post-produc-audiovisual">
+              <Link
+                as={NextLink}
+                fontSize="sm"
+                href="/tecnicos/post-produc-audiovisual"
+              >
                 PostProducción Audiovisual
               </Link>
             </ListItem>
             <ListItem>
-              <Link as={NextLink} fontSize="sm" href="/tecnicos/merc-digital-y-diseno">
+              <Link
+                as={NextLink}
+                fontSize="sm"
+                href="/tecnicos/merc-digital-y-diseno"
+              >
                 Mercadeo Digital y Diseño
               </Link>
             </ListItem>
             <ListItem>
-              <Link as={NextLink} fontSize="sm" href="/tecnicos/analisis-y-visuali-de-datos">
+              <Link
+                as={NextLink}
+                fontSize="sm"
+                href="/tecnicos/analisis-y-visuali-de-datos"
+              >
                 Análisis y Visualización de Datos
               </Link>
             </ListItem>
@@ -74,25 +94,37 @@ const Footer = () => {
               </Link>
             </ListItem>
             <ListItem>
-              <Link as={NextLink} fontSize="sm" href="/tecnicos/motion-graphics">
+              <Link
+                as={NextLink}
+                fontSize="sm"
+                href="/tecnicos/motion-graphics"
+              >
                 Motion Graphics
               </Link>
             </ListItem>
-            </UnorderedList>
-      </Box>
+          </UnorderedList>
+        </Box>
 
-      <Box>
-        <Heading as="h3" size="lg">¿Cómo llegar?</Heading>
-        <Text fontSize="sm">Parque La Libertad, 300 metros este y 100 metros norte del Palí de Fátima, Desamparados.</Text>
-        <Link as={NextLink} fontSize="sm" bg="gray.500" href="/contacto">
-          Ve cómo llegar
-        </Link>
-      </Box>
+        <Box>
+          <Heading as="h3" size="lg">
+            ¿Cómo llegar?
+          </Heading>
+          <Text fontSize="sm">
+            Parque La Libertad, 300 metros este y 100 metros norte del Palí de
+            Fátima, Desamparados.
+          </Text>
+          <Link as={NextLink} fontSize="sm" href="/contacto">
+            Ve cómo llegar
+          </Link>
+        </Box>
 
-      <Box>
-        <Heading as="h3" size="lg">Contacto</Heading>
-        <Text fontSize="sm">Teléfono: 2276-9400 ext.2031</Text>
-        <Text fontSize="sm">E-mail: admision.cetav@parquelalibertad.org</Text>
+        <Box>
+          <Heading as="h3" size="lg">
+            Contacto
+          </Heading>
+          <Text fontSize="sm">Teléfono: 2276-9400 ext.2031</Text>
+          <Text fontSize="sm">E-mail: admision.cetav@parquelalibertad.org</Text>
+        </Box>
       </Box>
     </Box>
   );
