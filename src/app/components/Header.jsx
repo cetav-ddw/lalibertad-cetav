69ef908eac8d76ff66f9199a330e42bf09caa380
Jose <leivajd@gmail.com>
2023-10-31 17:37:33 -0600

    Agrego logo y actualizo nav

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,28 @@
-import { Box } from '@chakra-ui/react';
-import Navigation from './navigation';
+import { Box, Flex } from '@chakra-ui/react';
+import Navigation from './Navigation';
 
 const Header = () => {
   return (
-    <Box as="header">
-      <Box maxW="1180px" ml="auto" mr="auto">
+    <Box as="header" py="5">
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        maxW="1180px"
+        ml="auto"
+        mr="auto"
+      >
+        {/* ToDo puede ser un link al homepage
+          Se necesita una imagen para retina
+        */}
+        <Box
+          backgroundImage="/images/triada-logo.png"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          width="400px"
+          height="90px"
+        />
         <Navigation />
-      </Box>
+      </Flex>
     </Box>
   );
 };
