b4a290efe63ca82dddc33b8d809a98fd82cb92f9
Jose <leivajd@gmail.com>
2023-10-31 11:02:16 -0600

    Se agrega componente de profesores

--- src/app/tecnicos/analisis-y-visuali-de-datos/page.js
+++ src/app/tecnicos/analisis-y-visuali-de-datos/page.js
@@ -2,7 +2,9 @@ import Link from 'next/link';
 import { Heading } from '@chakra-ui/react';
 import TestimonyList from '@/app/components/Testimonies/TestimonyList';
 import InscriptionBanner from '@/app/components/InscriptionBanner';
+import OurTeachers from '@/app/components/OurTeachers';
 import testimonies from '@/app/content/testimonies';
+import teachers from '@/app/content/teachers';
 
 export default function AnalisisDatosPage() {
   return (
@@ -10,6 +12,7 @@ export default function AnalisisDatosPage() {
       <Heading as="h1">ANALISIS Y VISUALIZACION DE DATOS PAGE</Heading>
       <Link href="/tecnicos">Tecnicos</Link>
       <TestimonyList testimonies={testimonies.web} />
+      <OurTeachers teacherList={teachers.web} />
       <InscriptionBanner />
     </>
   );
