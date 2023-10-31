3ef222e976b483c5bfc5277a7aa387a3e735d82a
Jose <leivajd@gmail.com>
2023-10-31 11:09:39 -0600

    Se agregan componentes a paginas de tecnicos

--- src/app/tecnicos/analisis-y-visuali-de-datos/page.js
+++ src/app/tecnicos/analisis-y-visuali-de-datos/page.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { Heading } from '@chakra-ui/react';
 import TestimonyList from '@/app/components/Testimonies/TestimonyList';
 import InscriptionBanner from '@/app/components/InscriptionBanner';
@@ -10,7 +9,6 @@ export default function AnalisisDatosPage() {
   return (
     <>
       <Heading as="h1">ANALISIS Y VISUALIZACION DE DATOS PAGE</Heading>
-      <Link href="/tecnicos">Tecnicos</Link>
       <TestimonyList testimonies={testimonies.web} />
       <OurTeachers teacherList={teachers.web} />
       <InscriptionBanner />
