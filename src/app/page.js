024474c7af9c2766a9407f22480e1c9a557dd69b
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-11-01 13:21:44 -0600

    elimino componentes repetidos

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,6 @@ export default function Home() {
       <InscriptionBanner />
       <LaboralFormation />
       <ImagePanel images={images} />
-      <InscriptionBanner />
-      <LaboralFormation />
     </>
   );
 }
