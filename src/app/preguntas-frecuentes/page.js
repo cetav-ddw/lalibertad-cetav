2390ff63d30c53ae0d88dad4d01debcc371e64cd
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2023-11-01 15:34:52 -0600

    Faq respuestas j (#69)
    Co-authored-by: MelanieLopezAlfaro <107006490+MelanieLopAl@users.noreply.github.com>
    Co-authored-by: OxCart00 <107891625+OxCart00@users.noreply.github.com>

--- src/app/preguntas-frecuentes/page.js
+++ src/app/preguntas-frecuentes/page.js
@@ -1,175 +1,188 @@
-'use client';
-import { Heading } from '@chakra-ui/react';
-import {
+import { 
+  Heading, 
+  ListItem, 
+  OrderedList,
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
-  Box,
+  Box, 
+  UnorderedList,
+  Link
 } from '@chakra-ui/react';
 import SectionHeading from '../components/SectionHeading';
 
 export default function Faq() {
   return (
     <>
-      <Heading as="h1" fontSize="xx-large">
-        Preguntas frecuentes
-      </Heading>
+      <Heading as="h1">Preguntas frecuentes</Heading>
       <SectionHeading title="Sobre el CETAV" />
-      <Accordion defaultIndex={[0]} allowMultiple>
-        <AccordionItem>
+      <Accordion allowMultiple>
+        <AccordionItem m={10} mb={20} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
                 ¿Cada cuánto tiempo se abre el proceso de admisión?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Cada dos años el CETAV realiza un proceso de reclutamiento, el
-            próximo dará inicio a partir de setiembre del 2023.
+
+          Cada dos años el CETAV realiza un proceso de reclutamiento, 
+          el próximo dará inicio a partir de setiembre del 2023.
+
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
 
       <SectionHeading title="Financiamiento" />
-      <Accordion defaultIndex={[0]} allowMultiple>
-        <AccordionItem>
+      <Accordion allowMultiple>
+        <AccordionItem m={10} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
                 ¿Cuál es el costo mensual de las carreras técnicas del CETAV?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            El costo mensual de la carrera técnica es de ₡100,000 en caso de
-            obtener la beca Empleate, gestionada en conjunto con el CETAV, el
-            50% del monto recibido es para pagar colegiatura y el 50% restante
-            para transporte y alimentación.
+
+          El costo mensual de la carrera técnica es de ₡100,000 en caso de obtener la beca Empleate, gestionada en conjunto con el CETAV, 
+          el 50% del monto recibido es para pagar colegiatura y el 50% restante para transporte y alimentación.
+
           </AccordionPanel>
         </AccordionItem>
-        <AccordionItem>
+        <AccordionItem m={10} mb={20} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Cuánto pagan los estudiantes que reciben beca de EMPLEATE?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Cuánto pagan los estudiantes que reciben beca de EMPLEATE?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            El 50% de la beca asignada es para pagar sus estudios en el CETAV y
-            el 50% restante para cubrir alimentación y transporte.
+
+          El 50% de la beca asignada es para pagar sus estudios en el CETAV y
+          el 50% restante para cubrir alimentación y transporte.
+
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
 
       <SectionHeading title="Sobre nuestro proceso de admisión" />
-      <Accordion defaultIndex={[0]} allowMultiple>
-        <AccordionItem>
+      <Accordion allowMultiple>
+        <AccordionItem m={10} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Cuál es el proceso para entrar al CETAV?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Cuál es el proceso para entrar al CETAV?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            <OrderedList>
+                <ListItem>Comprobar si cumplís con todos los requisitos y llenar el formulario: 
+                  <Link color="#313677" fontWeight={"bold"} title="link a los requisitos" href="#"> click aquí</Link></ListItem>
+                <ListItem>Si necesitás la beca de EMPLEATE debés llenar el siguiente formulario para iniciar el proceso: 
+                  <Link color="#313677" fontWeight={"bold"} title="link al formulario" href="#"> click aquí</Link></ListItem>
+                  <UnorderedList>
+                    <ListItem>Al llenar el formulario, en el punto IV. REFERENCIAS : ¿Ha sido referido por una institución o 
+                      actividad específica? debe elegir SÍ</ListItem>
+                    <ListItem>En “Indique el nombre de la institución”, especificar: CETAV Parque La Libertad.</ListItem>
+                  </UnorderedList>
+                <ListItem>Nosotros nos estaremos comunicando con vos para convocarte a las pruebas de admisión.</ListItem>
+            </OrderedList>
           </AccordionPanel>
         </AccordionItem>
-        <AccordionItem>
+        <AccordionItem m={10} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Cuáles son los requisitos para entrar al CETAV?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Cuáles son los requisitos para entrar al CETAV?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <UnorderedList>
+            <ListItem>Tener entre 17 y 27 años.</ListItem>
+            <ListItem>Bachillerato aprobado.</ListItem>
+            <ListItem>Conocimiento comprobable en Adobe Photoshop y Adobe Ilustrador básico o programas de edición de imagen similares. 
+              Si no los tienen, pueden llamar al Centro de Cómputo del Parque La Libertad al 2276-9400 ext 2092, 
+              donde estos cursos se imparten de manera regular.</ListItem>
+            <ListItem>En caso de extranjeros, tener su estatus migratorio al día.</ListItem>
+            <ListItem>Interés en aprender inglés.</ListItem>
+            <ListItem>Disponibilidad de tiempo completo.</ListItem>
+          </UnorderedList>
           </AccordionPanel>
         </AccordionItem>
-        <AccordionItem>
+        <AccordionItem m={10} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Cuál es la edad límite para poder entrar al CETAV?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Cuál es la edad límite para poder entrar al CETAV?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Entre 17 y 27 años.
           </AccordionPanel>
         </AccordionItem>
-        <AccordionItem>
+        <AccordionItem m={10} mb={20} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Puedo escoger qué materias matricular y en qué orden llevarlas?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Puedo escoger qué materias matricular y en qué orden llevarlas?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            No, es requisito llevar el bloque completo.
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
 
       <SectionHeading title="Inserción Laboral" />
-      <Accordion mb={10} defaultIndex={[0]} allowMultiple>
-        <AccordionItem>
+      <Accordion mb={10} allowMultiple>
+        <AccordionItem m={10} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿Cuál es el promedio de inserción laboral de egresados y
-                egresadas del CETAV?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿Cuál es el promedio de inserción laboral de egresados y egresadas del CETAV?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            El promedio oscila entre 80% y 90% de inserción laboral.
+
+          El promedio oscila entre 80% y 90% de inserción laboral.
+
           </AccordionPanel>
         </AccordionItem>
-        <AccordionItem>
+        <AccordionItem m={10} mb={20} borderStyle={"none"}>
           <h3>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                ¿En qué empresas trabajan los egresados de cada carrera?
+            <AccordionButton bg="#E1E7F8" p={4} color="#313677" rounded="lg" fontWeight={"bold"}>
+              <Box as="span" flex='1' textAlign='left'>
+              ¿En qué empresas trabajan los egresados de cada carrera?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h3>
           <AccordionPanel pb={4}>
-            Dependiendo del área de especialidad. Entre las principales destacan
-            canales de televisión como Repretel y Televisora de Costa Rica,
-            agencias digitales como Accenture, The Hangar, estudios de diseño y
-            arquitectura, agencias de publicidad, estudios de animación, entre
-            otras.
+
+          Dependiendo del área de especialidad. Entre las principales destacan canales de televisión como Repretel y Televisora de Costa Rica, 
+          agencias digitales como Accenture, The Hangar, estudios de diseño y arquitectura, agencias de publicidad, estudios de animación, entre otras.
+
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
-    </>
-  );
+
+    </>)
 }
