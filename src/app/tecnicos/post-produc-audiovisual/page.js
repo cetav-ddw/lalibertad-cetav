949ce089910e0a927f04f82c1f630bb2e2098e86
Jose <leivajd@gmail.com>
2023-10-31 21:47:51 -0600

    Banner de softskills

--- src/app/tecnicos/post-produc-audiovisual/page.js
+++ src/app/tecnicos/post-produc-audiovisual/page.js
@@ -5,6 +5,7 @@ import TestimonyList from '@/app/components/Testimonies/TestimonyList';
 import OurTeachers from '@/app/components/OurTeachers';
 import ExpectactionsBanner from '@/app/components/ExpectactionsBanner';
 import GoodToKnowBanner from '@/app/components/GoodToKnowBanner';
+import SoftSkillsBanner from '@/app/components/SoftSkillsBanner';
 import testimonies from '@/app/content/testimonies';
 import teachers from '@/app/content/teachers';
 import preInscription from '@/app/content/pre-inscription';
@@ -17,6 +18,7 @@ export default function PostProducionPage() {
       <GoodToKnowBanner topicList={preInscription.web} />
       <ExpectactionsBanner />
       <TestimonyList testimonies={testimonies.web} />
+      <SoftSkillsBanner />
       <OurTeachers teacherList={teachers.web} />
       <FaqBanner />
     </>
