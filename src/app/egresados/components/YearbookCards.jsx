39b52ba5426b0d749345b0952320e1868e594187
Adrián Zúñiga <az1767824@gmail.com>
2023-11-02 00:09:45 -0600

    Codigo comentado eliminado

--- src/app/egresados/components/YearbookCards.jsx
+++ src/app/egresados/components/YearbookCards.jsx
@@ -1,9 +1,7 @@
 'use client'
 import { Box, Image, Stack, WrapItem, Heading, Link } from '@chakra-ui/react';
-//import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Button } from '@chakra-ui/react';
 
 function YearbookCard({ generationImage, title, yearbookLink, reelLink }) {
-  //const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <WrapItem mb="16">
@@ -20,40 +18,17 @@ function YearbookCard({ generationImage, title, yearbookLink, reelLink }) {
             </Link>
 
             <Link target="_blank" fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' href={reelLink}>
-              {/*Eliminar este link en caso de utilizar el botón de reel */}
               Reel
             </Link>
 
-            {/*
-            <Button fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor'#313677' onClick={onOpen}>
-              Reel
-            </Button>
-            */}
           </Stack>
         </Box>
       </Box>
 
-      {/* EL modal está inhabilitado temporalmente */}
+      {/* TO DO para futuras mejoras: 
+      -Crear modal con Chakra UI para mostrar reel de Vimeo
+      -Reemplazar el Link del Reel por un botón para que abra el pop-up */}
     
-      {/*
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Reel</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <a href={reelLink}>{reelLink}</a>
-          </ModalBody>
-          <ModalFooter>
-            <Stack direction="column" spacing={4} align="center">
-              <Button fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' onClick={onClose}>
-                Cerrar
-              </Button>
-            </Stack>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      */}
     </WrapItem>
   );
 }
