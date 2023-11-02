90bbfeb92b7449d56791ed3ffe60902fbae6adfd
Adrián Zúñiga <az1767824@gmail.com>
2023-11-01 23:43:21 -0600

    Versión 1 y 2 de cards
    - Se dejó por default la versión 1 de los cards (con ambos links en el card).
    - La versión 2 está comentada, en caso de que se cambie de opinión es solo de descomentar y eliminar el "Link" del reel y descomentar el boton. para darle funcionalidad al modal.
    -Se agregaron los links hacia anuarios y reels de vimeo.

--- src/app/egresados/components/YearbookCards.jsx
+++ src/app/egresados/components/YearbookCards.jsx
@@ -1,26 +1,59 @@
-import { Box, Image, Link, Stack, WrapItem, Heading, LinkBox, LinkOverlay } from '@chakra-ui/react';
+'use client'
+import { Box, Image, Stack, WrapItem, Heading, Link } from '@chakra-ui/react';
+//import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Button } from '@chakra-ui/react';
 
-function YearbookCard({ generationImage, title, cardLink, reelLink }) {
+function YearbookCard({ generationImage, title, yearbookLink, reelLink }) {
+  //const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <WrapItem mb="16">
-      <LinkBox as="article">
-        <LinkOverlay href={cardLink}>
-          <Box maxW='350px' borderRadius='14px' overflow='hidden' position='relative'>
-            <Image src={`/images/${generationImage}`} alt='' filter='brightness(70%)' objectFit='cover' w="355.22px" h="314px" borderRadius="5px" />
+      <Box maxW='350px' borderRadius='14px' overflow='hidden' position='relative'>
+        <Image src={`/images/${generationImage}`} alt='' filter='brightness(70%)' objectFit='cover' w="355.22px" h="314px" borderRadius="5px" />
 
-            <Box p='6' position='absolute' bottom='0' left='0'>
-              <Heading as="h3" size="md" color="white" mb='2'>
-                {title}
-              </Heading>
-              <Stack direction='row' spacing={4} align='center'>
-                <Link border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' href={reelLink}>
-                  Reel
-                </Link>
-              </Stack>
-            </Box>
-          </Box>
-        </LinkOverlay>
-      </LinkBox>
+        <Box p='6' position='absolute' bottom='0' left='0'>
+          <Heading as="h3" size="md" color="white" mb='2'>
+            {title}
+          </Heading>
+          <Stack direction='row' spacing={4} align='center'>
+            <Link target="_blank" fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' href={yearbookLink}>
+              Anuario
+            </Link>
+
+            <Link target="_blank" fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' href={reelLink}>
+              {/*Eliminar este link en caso de utilizar el botón de reel */}
+              Reel
+            </Link>
+
+            {/*
+            <Button fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor'#313677' onClick={onOpen}>
+              Reel
+            </Button>
+            */}
+          </Stack>
+        </Box>
+      </Box>
+
+      {/* EL modal está inhabilitado temporalmente */}
+    
+      {/*
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Reel</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <a href={reelLink}>{reelLink}</a>
+          </ModalBody>
+          <ModalFooter>
+            <Stack direction="column" spacing={4} align="center">
+              <Button fontWeight="500" border='2px' px='4' py='1' backgroundColor='#e1e7f8' color='#313677' borderRadius='19px' borderColor='#313677' onClick={onClose}>
+                Cerrar
+              </Button>
+            </Stack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      */}
     </WrapItem>
   );
 }
--- src/app/egresados/page.js
+++ src/app/egresados/page.js
@@ -1,6 +1,7 @@
 import { Heading, Wrap } from '@chakra-ui/react';
 import YearbookCard from './components/YearbookCards';
 
+
 export default function Egresados() {
   return (
     <>
@@ -10,32 +11,32 @@ export default function Egresados() {
         <YearbookCard
           generationImage="generation_2013.png"
           title="1er Generación"
-          reelLink="#"
-          cardLink="#"
+          reelLink="https://vimeo.com/82142254"
+          yearbookLink="https://issuu.com/parquelalibertad/docs/_anuario_2013-4_cetav"
         />
         <YearbookCard
           generationImage="generation_2015.png"
           title="2da Generación"
-          reelLink="#"
-          cardLink="#"
+          reelLink="https://vimeo.com/149185849"
+          yearbookLink="https://issuu.com/parquelalibertad/docs/anuario_2015_segunda_generacio__n"
         />
         <YearbookCard
           generationImage="generation_2017.jpg"
           title="3er Generación"
-          reelLink="#"
-          cardLink="#"
+          reelLink="https://vimeo.com/247321835"
+          yearbookLink="https://issuu.com/parquelalibertad/docs/anurario_2017_tercera_generacion"
         /> 
         <YearbookCard
           generationImage="generation_2019.jpg"
           title="4ta Generación"
-          reelLink="#"
-          cardLink="#"
+          reelLink="https://vimeo.com/373945663"
+          yearbookLink="https://issuu.com/parquelalibertad/docs/anuraio_cetav_2019"
         />
         <YearbookCard
           generationImage="generation_2021.jpg"
           title="5ta Generación"
-          reelLink="#"
-          cardLink="#"
+          reelLink="https://vimeo.com/653480750"
+          yearbookLink="https://issuu.com/parquelalibertad/docs/anuario_2021"
         />
       </Wrap>
     </>
