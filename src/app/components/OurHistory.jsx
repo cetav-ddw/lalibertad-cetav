8afae489af738cc1b2614a5e7ba2a2cfd92b7666
Karina <karinadiaz1898@gmail.com>
2023-11-03 09:17:23 -0600

    Update OurHistory.jsx

--- src/app/components/OurHistory.jsx
+++ src/app/components/OurHistory.jsx
@@ -1,24 +1,22 @@
-'use client';
-
 import { Box, Flex, Text } from '@chakra-ui/react';
 import SectionHeading from '@/app/components/SectionHeading';
 
 const OurHistory = () => {
   return (
-    <Box mb="160">
+    <Box mb="16">
       <SectionHeading title="Nuestra Historia" />
       <Flex flexWrap="wrap" justifyContent="space-around">
         <Flex flexDirection="column" maxW="48%" mx="2" my="6">
           <Text color="#444444">
             {
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at massa elementum, tristique ipsum in, venenatis eros. Fusce sed volutpat elit. Praesent at felis et quam feugiat porttitor. Curabitur gravida turpis nec mi blandit auctor. Suspendisse magna tortor, luctus at auctor nec, semper sit amet lorem. Fusce dignissim ultricies diam non porta.'
+              'El Centro de Tecnología y Artes Visuales (CETAV) del Parque La Libertad es un centro de educación técnica enfocado en la inclusión social para la innovación, que busca fomentar el desarrollo humano y la formación de personas jóvenes a partir de opciones de especialización técnica alineadas con las demandas del mercado.'
             }
           </Text>
         </Flex>
         <Flex flexDirection="column" maxW="48%" mx="2" my="6">
           <Text color="#444444">
             {
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at massa elementum, tristique ipsum in, venenatis eros. Fusce sed volutpat elit. Praesent at felis et quam feugiat porttitor. Curabitur gravida turpis nec mi blandit auctor. Suspendisse magna tortor, luctus at auctor nec, semper sit amet lorem. Fusce dignissim ultricies diam non porta.'
+              'En el CETAV del Parque La Libertad, creemos que la empatía, la creatividad y la capacidad de trabajar en equipo, son competencias urgentes y diferenciadoras.'
             }
           </Text>
         </Flex>
