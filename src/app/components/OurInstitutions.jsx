910ed3905bd51feba7987a5889eb92d8c6f628ae
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-11-07 15:52:51 -0600

    PR updated

--- src/app/components/OurInstitutions.jsx
+++ src/app/components/OurInstitutions.jsx
@@ -7,14 +7,15 @@ const OurInstitutions = () => {
       wrap="wrap"
       justifyContent="center"
       alignItems="center"
-      w="100%"
-      // TODO meter este color en el theme
-      bg="#f0f0f0"
+      marginRight="-84px"
+      marginLeft="-84px"
+      // TODO mover este color al theme
+      bg="#f2f4fC"
       p="10px"
       mb="16px"
     >
-      {images.map((image, index) => (
-        <Box key={index} mx="5px">
+      {images.map((image) => (
+        <Box key={image.src} mx="5px">
           <Image src={image.src} alt={image.alt} w="100px" />
         </Box>
       ))}
