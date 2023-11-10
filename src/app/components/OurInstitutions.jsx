482baedf6a7fc54344fd139f40030e572eb175d6
MelanieLopezAlfaro <107006490+MelanieLopAl@users.noreply.github.com>
2023-11-10 11:04:04 -0600

    responsivehome
    acomode el "OurInstitutions" para que se acomode bien el responsive del home

--- src/app/components/OurInstitutions.jsx
+++ src/app/components/OurInstitutions.jsx
@@ -7,15 +7,12 @@ const OurInstitutions = () => {
       wrap="wrap"
       justifyContent="center"
       alignItems="center"
-      marginRight="-84px"
-      marginLeft="-84px"
-      // TODO mover este color al theme
       bg="#f2f4fC"
       p="10px"
       mb="16px"
     >
       {images.map((image) => (
-        <Box key={image.src} mx="5px">
+        <Box key={image.src} mx="10px" my="10px"> 
           <Image src={image.src} alt={image.alt} w="100px" />
         </Box>
       ))}
