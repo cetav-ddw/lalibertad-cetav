67d3821ff8ee186426bf9db57c4500e63c45c3bb
MelanieLopezAlfaro <107006490+MelanieLopAl@users.noreply.github.com>
2023-11-13 13:31:34 -0600

    responsive de la pag "Sobre nosotros"
    Se crea el responsive de la pag "Sobre nosotros".

--- src/app/components/AdvisoryBoard.jsx
+++ src/app/components/AdvisoryBoard.jsx
@@ -10,13 +10,13 @@ const AdvisoryBoard = () => {
       <SectionHeading title="Consejo Asesor" />
       <Flex flexWrap="wrap" justifyContent="space-around">
         <Flex flexDirection="column" maxW="48%" mx="2" my="6">
-          <Text fontSize="26px" color="#444444">
+          <Text fontSize={{ base: 'lg', md: '26px' }} color="#444444">
             {
               'El CETAV del Parque La Libertad, cuenta con la asesoría y acompañamiento de personas expertas y profesionales de la industria audiovisual; así como del sector de la educación técnica, que contribuyen a determinar las tendencias, necesidades y opciones que marcan el mercado y las futuras fuentes de empleo de nuestro estudiantado.'
             }
           </Text>
         </Flex>
-        <Flex flexDirection="column" maxW="48%" mx="2" my="6">
+        <Flex flexDirection="column" maxW="48%" mx="2" my="6" >
           <Box mb="16">
             <List maxW="980px" mx="auto" spacing={2}>
               <ListItem>
--- src/app/components/OurHistory.jsx
+++ src/app/components/OurHistory.jsx
@@ -3,21 +3,17 @@ import SectionHeading from '@/app/components/SectionHeading';
 
 const OurHistory = () => {
   return (
-    <Box mb="16">
+    <Box mb={{ base: '8', md: '16' }}>
       <SectionHeading title="Nuestra Historia" />
       <Flex flexWrap="wrap" justifyContent="space-around">
-        <Flex flexDirection="column" maxW="48%" mx="2" my="6">
-          <Text color="#444444">
-            {
-              'El Centro de Tecnología y Artes Visuales (CETAV) del Parque La Libertad es un centro de educación técnica enfocado en la inclusión social para la innovación, que busca fomentar el desarrollo humano y la formación de personas jóvenes a partir de opciones de especialización técnica alineadas con las demandas del mercado.'
-            }
+        <Flex flexDirection="column" maxW="48%" mx={{ base: '2', md: '2' }} my={{ base: '4', md: '6' }}>
+          <Text color="#444444" fontSize={{ base: 'sm', md: 'md' }}>
+            El Centro de Tecnología y Artes Visuales (CETAV) del Parque La Libertad es un centro de educación técnica enfocado en la inclusión social para la innovación, que busca fomentar el desarrollo humano y la formación de personas jóvenes a partir de opciones de especialización técnica alineadas con las demandas del mercado.
           </Text>
         </Flex>
-        <Flex flexDirection="column" maxW="48%" mx="2" my="6">
-          <Text color="#444444">
-            {
-              'En el CETAV del Parque La Libertad, creemos que la empatía, la creatividad y la capacidad de trabajar en equipo, son competencias urgentes y diferenciadoras.'
-            }
+        <Flex flexDirection="column" maxW="48%" mx={{ base: '2', md: '2' }}  my={{ base: '4', md: '6' }}>
+          <Text color="#444444" fontSize={{ base: 'sm', md: 'md' }}>
+            En el CETAV del Parque La Libertad, creemos que la empatía, la creatividad y la capacidad de trabajar en equipo, son competencias urgentes y diferenciadoras.
           </Text>
         </Flex>
       </Flex>
