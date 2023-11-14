feb15cc362b7708a254d7591515a26528cfdc1a3
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-11-14 17:13:42 -0600

    PageHeading_Responsive_Adjust

--- src/app/components/PageHeading.jsx
+++ src/app/components/PageHeading.jsx
@@ -3,10 +3,11 @@ import { Flex, Box, Heading, Text } from '@chakra-ui/react';
 const PHeading = ({ title, titleColor, text, imgSrc }) => {
   return (
     <Flex flexDirection={{ base: "column", md: "row" }}
-     mb={{ base: "12", md: "24" }}
-     justifyContent="space-between">
-      <Box width={{ base: "100%", md: "48%" }} mb={{ base: "8", md: 0 }}>
-        <Heading as="h1" fontSize={{ base: "3xl", md: "4xl" }} mb="4">
+      mb={{ base: "12", md: "24" }}
+      justifyContent="space-between"
+      alignItems='center'>
+      <Box width={{ base: "90%", md: "48%" }} mb={{ base: "8", md: 0 }}>
+        <Heading as="h1" fontSize={{ base: "5xl", md: "4xl" }} mb="4">
           <Text as="span" display="block">
             {title}
           </Text>
@@ -16,7 +17,7 @@ const PHeading = ({ title, titleColor, text, imgSrc }) => {
         </Heading>
 
         {text ? (
-          <Text fontSize={["md" ,"lg"]} color="#444444">
+          <Text fontSize={["md", "lg"]} color="#444444">
             {text}
           </Text>
         ) : null}
@@ -28,7 +29,7 @@ const PHeading = ({ title, titleColor, text, imgSrc }) => {
           backgroundRepeat="no-repeat"
           backgroundPosition="center"
           backgroundSize="cover"
-          width={{ base: "100%", md: "48%" }}
+          width={{ base: "80%", md: "48%" }}
           height={{ base: "300px", md: "auto" }}
           paddingTop={["50%", "350px"]}
           borderRadius="lg"
