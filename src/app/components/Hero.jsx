a7e19990f2dea13488968c7ee59d17a9bd76b7c4
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-11-17 16:03:17 -0600

    separe el preview del modal

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Flex, IconButton } from '@chakra-ui/react';
+import { FaPlay } from 'react-icons/fa';
+
+const Hero = ({ onOpen }) => {
+  const posterImageUrl = '/images/poster-CETAV-2023.png';
+  const localVideoUrl = '/videos/Reel-CETAV-2023.mp4';
+
+  return (
+    <Flex
+      mt="-70px"
+      mb="50px"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+    >
+      <video
+        width="65%"
+        loop
+        autoPlay
+        muted
+        style={{
+          boxShadow: '0 10px 10px rgba(0, 0, 0, 0.2)',
+          borderRadius: '10px',
+        }}
+        poster={posterImageUrl}
+      >
+        <source src={localVideoUrl} type="video/mp4" />
+      </video>
+      <IconButton
+        aria-label="Play Video"
+        icon={<FaPlay />}
+        size="lg"
+        //TODO meter este color al theme
+        colorScheme="purple"
+        variant="outline"
+        position="absolute"
+        zIndex="2"
+        onClick={onOpen}
+      />
+    </Flex>
+  );
+};
+
+export default Hero;
--- src/app/components/ModalVideo.jsx
+++ src/app/components/ModalVideo.jsx
@@ -9,51 +9,16 @@ import {
   ModalCloseButton,
   AspectRatio,
   useDisclosure,
-  Flex,
-  IconButton,
 } from '@chakra-ui/react';
-import { FaPlay } from 'react-icons/fa';
+import Hero from './Hero';
 
 const VideoModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const posterImageUrl = '/images/poster-CETAV-2023.png';
-  const localVideoUrl = '/videos/Reel-CETAV-2023.mp4';
   const vimeoVideoUrl = 'https://player.vimeo.com/video/808341150?h=0d39844605';
 
   return (
     <>
-      <Flex
-        mt="-70px"
-        mb="50px"
-        justifyContent="center"
-        alignItems="center"
-        position="relative"
-      >
-        <video
-          width="65%"
-          loop
-          autoPlay
-          muted
-          style={{
-            boxShadow: '0 10px 10px rgba(0, 0, 0, 0.2)',
-            borderRadius: '10px',
-          }}
-          poster={posterImageUrl}
-        >
-          <source src={localVideoUrl} type="video/mp4" />
-        </video>
-        <IconButton
-          aria-label="Play Video"
-          icon={<FaPlay />}
-          size="lg"
-          //TODO meter este color al theme
-          colorScheme="purple"
-          variant="outline"
-          position="absolute"
-          zIndex="2"
-          onClick={onOpen}
-        />
-      </Flex>
+      <Hero onOpen={onOpen} />
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent maxW="60vw">
