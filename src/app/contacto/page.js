f200b20ebea37754066a8deccbfcee31ff71f27b
AndreyrM01 <rjostin954@gmail.com>
2023-11-18 18:28:03 -0600

    Corrección del Heading de la pagina de contacto

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -1,14 +1,30 @@
+import { Heading, Text } from '@chakra-ui/react';
 import ContactDetail from '@/app/components/ContactDetail';
-import PHeading from '@/app/components/PageHeading';
-import headingInfo from '@/app/content/pageHeading';
 
 export default function Contacto() {
   return (
     <>
-      <PHeading
-        title={headingInfo.contacto.title}
-        titleColor={headingInfo.contacto.titleColor}
-      />
+       <Heading
+          as="h1"
+          fontSize="xx-large"
+          textAlign="center"
+          maxWidth="800px"
+          mx="auto"
+        >
+          Nuestra Información 
+          {/* TODO MOVER COLOR AL THEME */}
+          <Text
+            as="span"
+            display="block"
+            fontSize="xx-large"
+            mt="4"
+            fontWeight="800px"
+            mb="70px"
+            color="blueHighlightColor"
+          >
+            de Contacto
+          </Text>
+        </Heading>
       <ContactDetail />
     </>
   );
