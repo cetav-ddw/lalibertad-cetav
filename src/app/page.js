35bc15a394579782f72000ce06159bd5afeac3db
Karina Díaz <107014055+DefArs@users.noreply.github.com>
2023-11-22 10:02:05 -0600

    update texts and logo (#171)
    Co-authored-by: Jose <leivajd@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import images from '@/app/content/imageData';
 import CourseList from '@/app/components/CourseList';
 import cursos from '@/app/content/cursosData';
 import OurAllies from '@/app/components/OurAllies';
-import ExploreOtherCareers from '@/app/components/ExploreOtherCareers';
 import OurInstitutions from '@/app/components/OurInstitutions';
 import HeroHeader from '@/app/components/HeroHeader';
 
@@ -43,11 +42,6 @@ export default function Home() {
       <InscriptionBanner />
       <LaboralFormation />
       <OurAllies images={images} />
-      <ExploreOtherCareers
-        isHomePage={true}
-        items={2}
-        title={'Hecha un vistazo a nuestros cursos'}
-      />
     </>
   );
 }
