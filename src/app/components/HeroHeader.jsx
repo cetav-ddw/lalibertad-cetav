1be71fff6790a69c9624d30ae8c1918cb00b4ab3
Jose <leivajd@gmail.com>
2023-11-22 10:03:43 -0600

    Home hero

--- src/app/components/HeroHeader.jsx
+++ src/app/components/HeroHeader.jsx
@@ -1,17 +1,51 @@
 'use client';
 
-import { useDisclosure } from '@chakra-ui/react';
+import {
+  useDisclosure,
+  Heading,
+  Text,
+  Flex,
+  IconButton,
+} from '@chakra-ui/react';
 import HeroPreview from '@/app/components/HeroPreview';
 import VideoModal from '@/app/components/VideoModal';
+import { FaPlay } from 'react-icons/fa';
 
 const HeroHeader = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
-      <HeroPreview onOpen={onOpen} />
+    <Flex maxW="1180px" ml="auto" mr="auto">
+      <Flex flexDirection="column" flex="1 0 55%" pr="12">
+        <Heading as="h1" fontSize="x-large" mb="4">
+          Centro de Tecnología y Artes Visuales (CETAV)
+        </Heading>
+        <Text fontSize="lg" mb="4" textShadow="md">
+          Somos un centro de formación para el empleo, con nosotros podés
+          acceder a programas técnicos y cursos libres para desarrollar tu
+          perfil profesional.
+        </Text>
+        <Text fontSize="lg" textShadow="md">
+          Creamos alianzas público-privadas para que nuestros cursos sean
+          accesibles y nuestra oferta esté alineada a las demandas del mercado.
+        </Text>
+      </Flex>
+      <Flex position="relative" justifyContent="center" alignItems="center">
+        <IconButton
+          aria-label="Play Video"
+          icon={<FaPlay />}
+          size="lg"
+          //TODO meter este color al theme
+          variant="outline"
+          position="absolute"
+          zIndex="2"
+          onClick={onOpen}
+          color="#7846BF"
+        />
+        <HeroPreview onOpen={onOpen} />
+      </Flex>
       <VideoModal isOpen={isOpen} onClose={onClose} />
-    </>
+    </Flex>
   );
 };
 
--- src/app/components/HeroPreview.jsx
+++ src/app/components/HeroPreview.jsx
@@ -1,43 +1,20 @@
-import { Flex, IconButton } from '@chakra-ui/react';
-import { FaPlay } from 'react-icons/fa';
-
-const HeroPreview = ({ onOpen }) => {
+const HeroPreview = () => {
   const posterImageUrl = '/images/poster-CETAV-2023.png';
   const localVideoUrl = '/videos/Reel-CETAV-2023.mp4';
 
   return (
-    <Flex
-      mt="-70px"
-      mb="50px"
-      justifyContent="center"
-      alignItems="center"
-      position="relative"
+    <video
+      loop
+      autoPlay
+      muted
+      style={{
+        boxShadow: '0 10px 10px rgba(0, 0, 0, 0.2)',
+        borderRadius: '10px',
+      }}
+      poster={posterImageUrl}
     >
-      <video
-        width="65%"
-        loop
-        autoPlay
-        muted
-        style={{
-          boxShadow: '0 10px 10px rgba(0, 0, 0, 0.2)',
-          borderRadius: '10px',
-        }}
-        poster={posterImageUrl}
-      >
-        <source src={localVideoUrl} type="video/mp4" />
-      </video>
-      <IconButton
-        aria-label="Play Video"
-        icon={<FaPlay />}
-        size="lg"
-        //TODO meter este color al theme
-        colorScheme="purple"
-        variant="outline"
-        position="absolute"
-        zIndex="2"
-        onClick={onOpen}
-      />
-    </Flex>
+      <source src={localVideoUrl} type="video/mp4" />
+    </video>
   );
 };
 
