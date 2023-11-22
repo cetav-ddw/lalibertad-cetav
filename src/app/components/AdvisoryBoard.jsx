efd664c83ebaafef1720302f7f722a961c80fd94
Jose <leivajd@gmail.com>
2023-11-22 09:59:30 -0600

    feedback

--- src/app/components/AdvisoryBoard.jsx
+++ src/app/components/AdvisoryBoard.jsx
@@ -6,13 +6,19 @@ import SectionHeading from '@/app/components/SectionHeading';
 
 const AdvisoryBoard = () => {
   return (
-    <Box mb="16" p={{ base: 4, md: 6 }} textAlign="center">
+    <Box mb="16" p={{ base: 4, md: 6 }}>
       <SectionHeading title="Consejo Asesor" />
-      <Text maxWidth="1000px" margin="0 auto" pb="30px" fontSize={{ base: 'md', md: 'lg' }}>
-        Se cuenta con equipo de personas en puestos importantes del estado y empresa
-        privada, que guían la ruta de trabajo del CETAV y sus alcances. Es un Consejo Asesor compuesto por:
+      <Text mb="8" fontSize={{ base: 'md', md: 'lg' }}>
+        Se cuenta con equipo de personas en puestos importantes del estado y
+        empresa privada, que guían la ruta de trabajo del CETAV y sus alcances.
+        El Consejo Asesor está compuesto por:
       </Text>
-      <List maxW="500px" mx="auto" spacing={2} textAlign="left">
+      <List
+        mb="8"
+        spacing={2}
+        textAlign="left"
+        fontSize={{ base: 'md', md: 'lg' }}
+      >
         <ListItem>
           <ListIcon as={MdCheckCircle} color="#313677" />
           Representante del Ministerio de Trabajo y Seguridad Social
@@ -46,9 +52,10 @@ const AdvisoryBoard = () => {
           Representantes de la comunidad SOY CETAV
         </ListItem>
       </List>
-      <Text maxWidth="1000px" margin="0 auto" pt="30px" fontSize={{ base: 'md', md: 'lg' }}>
-        Aunado a lo anterior, contamos con un equipo de expertos en cada área, quienes supervisan
-        la ejecución y contenidos del programa, para que cumpla con los requerimientos de las respectivas industrias.
+      <Text fontSize={{ base: 'md', md: 'lg' }}>
+        Aunado a lo anterior, contamos con un equipo de expertos en cada área,
+        quienes supervisan la ejecución y contenidos del programa, para que
+        cumpla con los requerimientos de las respectivas industrias.
       </Text>
     </Box>
   );
