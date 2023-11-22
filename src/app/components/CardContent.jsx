a9bb371361378227362ccf75c4162bcc5ec3d985
AndreyrM01 <rjostin954@gmail.com>
2023-11-22 14:10:11 -0600

    Nuevo diseño y agrego las imagenes junto a los textos

--- src/app/components/CardContent.jsx
+++ src/app/components/CardContent.jsx
@@ -1,88 +1,149 @@
 'use client';
-import { Box, Text, Link } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Link,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Button,
+  Heading,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
+import SectionHeading from './SectionHeading';
 
 const CardContent = () => {
   return (
     <>
+      <SectionHeading title="Nuestros programas" />
       <Box
         color="#000000"
-        my="50px"
+        mb="50px"
         display="flex"
         alignItems="center"
         justifyContent="center"
       >
-        <NextLink href="/tecnicos" passHref>
-          <Link>
-            <Box
-              w="350px"
-              h="198px"
-              border="2px solid #000000"
-              backgroundImage="/images/tecnicos.png"
-              backgroundSize="cover"
-              backgroundRepeat="no-repeat"
-              mx="10px"
-            >
-              <Text
-                color="#ffffff"
-                pos="relative"
-                top="160px"
-                left="10px"
-                fontSize="22px"
-              >
-                Programas Técnicos
+        <Card maxW="xs" h="450px" bg="boxBgColor" mx="4px">
+          <Image
+            src="/images/tecnicos.jpg"
+            alt="Card Tecnicos"
+            borderRadius="lg"
+            objectFit="cover"
+            h="10rem"
+          />
+          <CardBody>
+            <Stack mt="2" spacing="3">
+              <Heading size="md">Programas técnicos</Heading>
+              <Text>
+                Formación en áreas del Arte y Tecnología para el empleo.
+                Programas enfocados en el desarrollo de competencias técnicas,
+                inglés y habilidades socio emocionales.
               </Text>
-            </Box>
-          </Link>
-        </NextLink>
-
-        <NextLink href="/cursos-libres" passHref>
-          <Link>
-            <Box
-              w="350px"
-              h="198px"
-              border="2px solid #000000"
-              backgroundImage="/images/cursosLibres.png"
-              backgroundSize="cover"
-              backgroundRepeat="no-repeat"
-              mx="10px"
-            >
-              <Text
-                color="#ffffff"
-                pos="relative"
-                top="160px"
-                left="10px"
-                fontSize="22px"
-              >
-                Cursos Libres de Diseño
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <NextLink href="/tecnicos">
+              <Link>
+                <Button
+                  variant="solid"
+                  w="200px"
+                  display="flex"
+                  justifyContent="space-evenly"
+                  _hover={{
+                    bg: 'btnHover',
+                  }}
+                  pos="relative"
+                  bottom="15px"
+                >
+                  <Text>Aprende más</Text>
+                  <FaArrowRight />
+                </Button>
+              </Link>
+            </NextLink>
+          </CardFooter>
+        </Card>
+        <Card maxW="xs" h="450px" bg="boxBgColor" mx="4px">
+          <Image
+            src="/images/cursos-libres.jpg"
+            alt="Card Cursos Libres"
+            borderRadius="lg"
+            objectFit="cover"
+            h="10rem"
+          />
+          <CardBody>
+            <Stack mt="2" spacing="3">
+              <Heading size="md"> Cursos Libres de Diseño</Heading>
+              <Text>
+                Actualización profesional en áreas de diseño y redes sociales,
+                ofrecemos los cursos en modalidad presencial y virtual para tu
+                beneficio.
               </Text>
-            </Box>
-          </Link>
-        </NextLink>
-
-        <NextLink href="/cursos-libres/#cursosComputo" passHref>
-          <Link>
-            <Box
-              w="350px"
-              h="198px"
-              border="2px solid #000000"
-              backgroundImage="/images/centrosComputo.png"
-              backgroundSize="cover"
-              backgroundRepeat="no-repeat"
-              mx="10px"
-            >
-              <Text
-                color="#ffffff"
-                pos="relative"
-                top="160px"
-                left="10px"
-                fontSize="22px"
-              >
-                Centros de Cómputo
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <NextLink href="/cursos-libres">
+              <Link>
+                <Button
+                  variant="solid"
+                  w="200px"
+                  display="flex"
+                  justifyContent="space-evenly"
+                  _hover={{
+                    bg: 'btnHover',
+                  }}
+                  pos="relative"
+                  top="3px"
+                >
+                  <Text>Aprende más</Text>
+                  <FaArrowRight />
+                </Button>
+              </Link>
+            </NextLink>
+          </CardFooter>
+        </Card>
+        <Card maxW="xs" h="450px" bg="boxBgColor" mx="4px">
+          <Image
+            src="/images/centrosComputo.jpg"
+            alt="Card Centros de Computo"
+            borderRadius="lg"
+            objectFit="cover"
+            h="10rem"
+          />
+          <CardBody>
+            <Stack mt="1" spacing="3">
+              <Heading size="md">Centros de Cómputo</Heading>
+              <Text>
+                Desarrollo de habilidades en cómputo que abarcan desde la
+                navegación hasta el uso de herramientas exigidas por las
+                empresas como lo son excel y Power BI, ofrecemos modalidad
+                virtual y presencial.
               </Text>
-            </Box>
-          </Link>
-        </NextLink>
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <NextLink href="/cursos-libres/#cursosComputo">
+              <Link>
+                <Button
+                  variant="solid"
+                  w="200px"
+                  display="flex"
+                  justifyContent="space-evenly"
+                  _hover={{
+                    bg: 'btnHover',
+                  }}
+                  pos="relative"
+                  bottom="35px"
+                >
+                  <Text>Aprende más</Text>
+                  <FaArrowRight />
+                </Button>
+              </Link>
+            </NextLink>
+          </CardFooter>
+        </Card>
       </Box>
     </>
   );
--- src/theme/foundations/colors.js
+++ src/theme/foundations/colors.js
@@ -2,6 +2,7 @@ const colors = {
   boxBgColor: '#E1E7F8',
   textColor: '#313677',
   blueHighlightColor: '#427FDF',
+  btnHover:"#3D45B8",
 };
 
 export default colors;
