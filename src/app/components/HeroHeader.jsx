168418d25430dc4ef1ff98105fb8bc5be54132c2
Jason Umaña <jason1202u@gmail.com>
2023-11-23 13:27:54 -0600

    add responsive to herobanner in home

--- src/app/components/HeroHeader.jsx
+++ src/app/components/HeroHeader.jsx
@@ -19,23 +19,30 @@ const HeroHeader = () => {
       maxW="1180px"
       ml="auto"
       mr="auto"
-      flexDirection={{ base: 'column', md: 'row' }}
+      flexDirection={{ base: 'column', lg: 'row' }}
     >
       <Flex flexDirection="column" flex="1 0 55%" pr="12">
-        <Heading as="h1" fontSize="x-large" mb="4">
+        <Heading as="h1" fontSize="x-large" mb="4" width="90%" m="auto" p="3">
           Centro de Tecnología y Artes Visuales (CETAV)
         </Heading>
-        <Text fontSize="lg" mb="4" textShadow="md">
+        <Text fontSize="lg" mb="4" textShadow="md" width="90%" m="auto" p="3">
           Somos un centro de formación para el empleo, con nosotros podés
           acceder a programas técnicos y cursos libres para desarrollar tu
           perfil profesional.
         </Text>
-        <Text fontSize="lg" textShadow="md">
+        <Text fontSize="lg" textShadow="md" width="90%" m="auto" p="3">
           Creamos alianzas público-privadas para que nuestros cursos sean
           accesibles y nuestra oferta esté alineada a las demandas del mercado.
         </Text>
       </Flex>
-      <Flex position="relative" justifyContent="center" alignItems="center">
+      <Flex
+        position="relative"
+        justifyContent="center"
+        alignItems="center"
+        width="90%"
+        m="auto"
+        p="2"
+      >
         <IconButton
           aria-label="Play Video"
           icon={<FaPlay />}
