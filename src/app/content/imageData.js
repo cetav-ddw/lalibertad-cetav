52f9ebf84a972049c508931f98f700c52c562eb2
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-11-29 22:04:58 -0600

    Added socios de trabajo images

--- src/app/content/imageData.js
+++ src/app/content/imageData.js
@@ -2,88 +2,128 @@
 
 const images = [
   {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    url: '/images/logo_mision2.jpg',
+    description: 'Misión 2',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
-  },
-  {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
-  },
-  {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
-  },
-  {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-within-horizontal.png',
+    description: 'Within Horizontal',
   },
   {
     url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    description: 'Amazon',
+  },
+  {
+    url: '/images/logo-vmcj.jpg',
+    description: 'Viceministerio de Juventud',
+  },
+  {
+    url: '/images/logo-sapiens.png',
+    description: 'Sapiens Development',
+  },
+  {
+    url: '/images/logo-utn.svg',
+    description: 'Universidad Técnica Nacional',
+  },
+  {
+    url: '/images/logo-pupila.png',
+    description: 'Pupila',
+  },
+  {
+    url: '/images/logo-osopez.jpg',
+    description: 'Osopez',
+  },
+  {
+    url: '/images/logo-pulse.png',
+    description: 'Pulse',
   },
   {
     url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    description: 'Municipalidad de Desamparados',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-museo-arte-costarricense.png',
+    description: 'Museo de Arte Costarricense',
   },
   {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-muni-perez-zeledon.png',
+    description: 'Municipalidad de Perez Zeledon',
   },
   {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    url: '/images/logo-mnc.png',
+    description: 'Marco Nacional de Cualificaciones',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-mcj.svg',
+    description: 'Ministerio de Cultura y Juventud',
   },
   {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-maucc2023.png',
+    description: 'Mercado Audiovisual Centroamérica & Caribe',
   },
   {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    url: '/images/logo-marviva.png',
+    description: 'Mar Viva',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-interaction.png',
+    description: 'Interaction Agencia de Publicidad',
   },
   {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-innovacion-creatividad-cr.png',
+    description: 'Laboratorio de Creatividad e Innovación',
   },
   {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    url: '/images/logo-IMC.png',
+    description: 'imc',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-fundaula.png',
+    description: 'Plataforma Digital Fundación Accenture',
   },
   {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-fundación-forte.png',
+    description: 'Fundación Forte',
   },
   {
-    url: '/images/Logo-MunicipalidadDesamparados.png',
-    description: 'Descripción de la imagen 1',
+    url: '/images/logo-drupal.png',
+    description: 'Drupal',
   },
   {
-    url: '/images/Logo-BancoNacional.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-crusa-somos-comunidad-fc.png',
+    description: 'Crusa Somos Comunidad FC',
   },
   {
-    url: '/images/Logo-Amazon.png',
-    description: 'Descripción de la imagen 2',
+    url: '/images/logo-crfic.jpg',
+    description: 'Festival Internacional de Cine Costa Rica',
+  },
+  {
+    url: '/images/logo-centro-cine.svg',
+    description: 'Centro de Cine Costa Rica',
+  },
+  {
+    url: '/images/logo-caramba-rental2.png',
+    description: 'Caramba Rental',
+  },
+  {
+    url: '/images/logo-animation-cr.png',
+    description: 'Animation Costa Rica',
+  },
+  {
+    url: '/images/logo-american-tower.png',
+    description: 'American Tower',
+  },
+  {
+    url: '/images/logo-accenture.png',
+    description: 'Accenture',
+  },
+  {
+    url: '/images/logo-p&g.png',
+    description: 'P&G',
+  },
+  {
+    url: '/images/logo-stit-cybersecurity.png',
+    description: 'Stit Cybersecurity',
   },
 ];
 export default images;
