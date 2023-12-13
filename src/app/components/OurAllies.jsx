b6e88dfa5409f5ecaec7c9a2cee5f9b2d801bee0
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-12-13 01:08:08 -0600

    update OurAllies.jsx

--- src/app/components/OurAllies.jsx
+++ src/app/components/OurAllies.jsx
@@ -11,8 +11,6 @@ const OurAllies = ({ images }) => {
         {images.map((imageObj, index) => (
           <Box
             key={index}
-            /* height={{ md: '100px', lg: '150px' }}
-            width={{ md: '100px', lg: '150px' }} */
             height={{ base: '60px', md: '120px' }}
             width={{ base: '60px', md: '175px' }}
             mr={{ base: '20px', md: index % 7 === 6 ? '0' : '25px' }}
