695a64d46bb5894f4ddd650c907dfdf0f4574586
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-12-16 03:55:24 -0600

    Update order imageData.js, institutionsData.js

--- src/app/content/imageData.js
+++ src/app/content/imageData.js
@@ -1,10 +1,4 @@
-// TODO ACTUALIZAR TODAS LAS IMAGENES A LAS REALES E DESCRIPCION
-
 const images = [
-  {
-    url: '/images/logo_mision2.jpg',
-    description: 'Misión 2',
-  },
   {
     url: '/images/logo-within-horizontal.png',
     description: 'Within Horizontal',
@@ -14,32 +8,40 @@ const images = [
     description: 'Amazon',
   },
   {
-    url: '/images/logo-vmcj.jpg',
-    description: 'Viceministerio de Juventud',
+    url: '/images/logo-caramba-rental2.png',
+    description: 'Caramba Rental',
+  },
+  {
+    url: '/images/logo-interaction.png',
+    description: 'Interaction Agencia de Publicidad',
+  },
+  {
+    url: '/images/logo-fundaula.png',
+    description: 'Plataforma Digital Fundación Accenture',
   },
   {
     url: '/images/logo-sapiens.png',
     description: 'Sapiens Development',
   },
   {
-    url: '/images/logo-utn.svg',
-    description: 'Universidad Técnica Nacional',
+    url: '/images/logo-accenture.png',
+    description: 'Accenture',
   },
   {
-    url: '/images/logo-pupila.png',
-    description: 'Pupila',
+    url: '/images/logo-vmcj.jpg',
+    description: 'Viceministerio de Juventud',
   },
   {
-    url: '/images/logo-osopez.jpg',
-    description: 'Osopez',
-  },
-  {
-    url: '/images/logo-pulse.png',
-    description: 'Pulse',
+    url: '/images/logo_mision2.jpg',
+    description: 'Misión 2',
   },
   {
     url: '/images/Logo-MunicipalidadDesamparados.png',
     description: 'Municipalidad de Desamparados',
+  },
+  {
+    url: '/images/logo-utn.svg',
+    description: 'Universidad Técnica Nacional',
   },
   {
     url: '/images/logo-museo-arte-costarricense.png',
@@ -58,64 +60,60 @@ const images = [
     description: 'Mar Viva',
   },
   {
-    url: '/images/logo-interaction.png',
-    description: 'Interaction Agencia de Publicidad',
-  },
-  {
-    url: '/images/logo-innovacion-creatividad-cr.png',
-    description: 'Laboratorio de Creatividad e Innovación',
-  },
-  {
-    url: '/images/logo-IMC.png',
-    description: 'imc',
-  },
-  {
-    url: '/images/logo-fundaula.png',
-    description: 'Plataforma Digital Fundación Accenture',
+    url: '/images/logo-american-tower.png',
+    description: 'American Tower',
   },
   {
     url: '/images/logo-fundación-forte.png',
     description: 'Fundación Forte',
   },
   {
-    url: '/images/logo-drupal.png',
-    description: 'Drupal',
+    url: '/images/logo-stit-cybersecurity.png',
+    description: 'Stit Cybersecurity',
   },
   {
     url: '/images/logo-crusa-somos-comunidad-fc.png',
     description: 'Crusa Somos Comunidad FC',
   },
   {
-    url: '/images/logo-crfic.jpg',
-    description: 'Festival Internacional de Cine Costa Rica',
-  },
-  {
     url: '/images/logo-centro-cine.svg',
     description: 'Centro de Cine Costa Rica',
-  },
-  {
-    url: '/images/logo-caramba-rental2.png',
-    description: 'Caramba Rental',
   },
   {
     url: '/images/logo-animation-cr.png',
     description: 'Animation Costa Rica',
   },
   {
-    url: '/images/logo-american-tower.png',
-    description: 'American Tower',
+    url: '/images/logo-drupal.png',
+    description: 'Drupal',
   },
   {
-    url: '/images/logo-accenture.png',
-    description: 'Accenture',
+    url: '/images/logo-innovacion-creatividad-cr.png',
+    description: 'Laboratorio de Creatividad e Innovación',
+  },
+  {
+    url: '/images/logo-osopez.jpg',
+    description: 'Osopez',
   },
   {
     url: '/images/logo-p&g.png',
     description: 'P&G',
   },
   {
-    url: '/images/logo-stit-cybersecurity.png',
-    description: 'Stit Cybersecurity',
+    url: '/images/logo-pupila.png',
+    description: 'Pupila',
+  },
+  {
+    url: '/images/logo-pulse.png',
+    description: 'Pulse',
+  },
+  {
+    url: '/images/logo-IMC.png',
+    description: 'imc',
+  },
+  {
+    url: '/images/logo-crfic.jpg',
+    description: 'Festival Internacional de Cine Costa Rica',
   },
 ];
 export default images;
--- src/app/content/institutionsData.js
+++ src/app/content/institutionsData.js
@@ -1,6 +1,10 @@
-// TODO hay que actulizar esto con datos reales
-
 const images = [
+  {
+    src: '/images/Logo-Agencia-Nacional-Empleo.png',
+    alt: 'Agencia Nacional de Empleo',
+  },
+  { src: '/images/Logo-CINDE.png', alt: 'CINDE' },
+  { src: '/images/Logo-caiac.png', alt: 'CAIAC' },
   {
     src: '/images/Logo-MCJ.png',
     alt: 'Ministerio de Cultura y Juventud',
@@ -9,24 +13,18 @@ const images = [
     src: '/images/Logo-Empleate.png',
     alt: 'Empléate',
   },
+  { src: '/images/Logo-CAMTIC.png', alt: 'CAMTIC' },
   {
-    src: '/images/Logo-Agencia-Nacional-Empleo.png',
-    alt: 'Agencia Nacional de Empleo',
+    src: '/images/Logo-Cybersec.png',
+    alt: 'Miembro honorario Cybersec',
   },
   {
     src: '/images/Logo-MTSS.png',
     alt: 'Ministerio de trabajo y Seguridad Social',
   },
-  { src: '/images/Logo-caiac.png', alt: 'CAIAC' },
-  { src: '/images/Logo-CAMTIC.png', alt: 'CAMTIC' },
-  { src: '/images/Logo-CINDE.png', alt: 'CINDE' },
   { src: '/images/Logo-Procomer.png', alt: 'Procomer' },
   { src: '/images/Logo-PEN.png', alt: 'Programa Estado de la Nación' },
   { src: '/images/Logo-Cisco.png', alt: 'CISCO' },
-  {
-    src: '/images/Logo-Cybersec.png',
-    alt: 'Miembro honorario Cybersec',
-  },
 ];
 
 export default images;
